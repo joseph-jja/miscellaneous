bfcfc6faf7c983480a6e645ddea4e163a0370320
joe <josepha48@yahoo.com>
2019-11-11 15:53:52 -0800

    add all numbers -icky but working

--- time-n-weather/numbers/numbers.c
+++ time-n-weather/numbers/numbers.c
@@ -136,3 +136,209 @@ void writeThree(int x, int y) {
     move(yp, xp);
     addch(ACS_LRCORNER);
 }
+
+void writeFour(int x, int y) {
+
+    int xp = x;
+    int yp = y;
+
+    move(yp, xp);
+
+    addch(ACS_TTEE);
+    xp++;
+    xp++;
+    move(yp, xp);
+    addch(ACS_TTEE);
+
+    xp = x;
+    yp++;
+    move(yp, xp);
+    addch(ACS_LLCORNER);
+    xp++;
+    move(yp, xp);
+    addch(ACS_HLINE);
+    xp++;
+    move(yp, xp);
+    addch(ACS_PLUS);
+
+    xp = x;
+    yp++;
+    xp++;
+    xp++;
+    move(yp, xp);
+    addch(ACS_BTEE);
+}
+
+void writeFive(int x, int y) {
+
+    int xp = x;
+    int yp = y;
+
+    move(yp, xp);
+
+    addch(ACS_ULCORNER);
+    xp++;
+    move(yp, xp);
+    addch(ACS_HLINE);
+
+    xp = x;
+    yp++;
+    move(yp, xp);
+    addch(ACS_LLCORNER);
+    xp++;
+    move(yp, xp);
+    addch(ACS_HLINE);
+    xp++;
+    move(yp, xp);
+    addch(ACS_URCORNER);
+
+    xp = x;
+    yp++;
+    //move(yp, xp);
+    //addch(ACS_LLCORNER);
+    xp++;
+    move(yp, xp);
+    addch(ACS_HLINE);
+    xp++;
+    move(yp, xp);
+    addch(ACS_LRCORNER);
+}
+
+void writeSix(int x, int y) {
+
+    int xp = x;
+    int yp = y;
+
+    move(yp, xp);
+
+    addch(ACS_ULCORNER);
+    xp++;
+    move(yp, xp);
+    addch(ACS_HLINE);
+
+    xp = x;
+    yp++;
+    move(yp, xp);
+    addch(ACS_LTEE);
+    xp++;
+    move(yp, xp);
+    addch(ACS_HLINE);
+    xp++;
+    move(yp, xp);
+    addch(ACS_URCORNER);
+
+    xp = x;
+    yp++;
+    move(yp, xp);
+    addch(ACS_LLCORNER);
+    xp++;
+    move(yp, xp);
+    addch(ACS_HLINE);
+    xp++;
+    move(yp, xp);
+    addch(ACS_LRCORNER);
+}
+
+void writeSeven(int x, int y) {
+
+    int xp = x;
+    int yp = y;
+
+    move(yp, xp);
+
+    addch(ACS_ULCORNER);
+    xp++;
+    move(yp, xp);
+    addch(ACS_HLINE);
+    xp++;
+    move(yp, xp);
+    addch(ACS_URCORNER);
+
+    xp = x;
+    yp++;
+    xp++;
+    xp++;
+    move(yp, xp);
+    addch(ACS_VLINE);
+
+    xp = x;
+    yp++;
+    xp++;
+    xp++;
+    move(yp, xp);
+    addch(ACS_BTEE);
+}
+
+void writeEight(int x, int y) {
+
+    int xp = x;
+    int yp = y;
+
+    move(yp, xp);
+
+    addch(ACS_ULCORNER);
+    xp++;
+    move(yp, xp);
+    addch(ACS_HLINE);
+    xp++;
+    move(yp, xp);
+    addch(ACS_URCORNER);
+
+    xp = x;
+    yp++;
+    move(yp, xp);
+    addch(ACS_LTEE);
+    xp++;
+    move(yp, xp);
+    addch(ACS_HLINE);
+    xp++;
+    move(yp, xp);
+    addch(ACS_RTEE);
+
+    xp = x;
+    yp++;
+    move(yp, xp);
+    addch(ACS_LLCORNER);
+    xp++;
+    move(yp, xp);
+    addch(ACS_HLINE);
+    xp++;
+    move(yp, xp);
+    addch(ACS_LRCORNER);
+}
+
+void writeNine(int x, int y) {
+
+    int xp = x;
+    int yp = y;
+
+    move(yp, xp);
+
+    addch(ACS_ULCORNER);
+    xp++;
+    move(yp, xp);
+    addch(ACS_HLINE);
+    xp++;
+    move(yp, xp);
+    addch(ACS_URCORNER);
+
+    xp = x;
+    yp++;
+    move(yp, xp);
+    addch(ACS_LLCORNER);
+    xp++;
+    move(yp, xp);
+    addch(ACS_HLINE);
+    xp++;
+    move(yp, xp);
+    addch(ACS_RTEE);
+
+    xp = x;
+    yp++;
+    xp++;
+    move(yp, xp);
+    addch(ACS_HLINE);
+    xp++;
+    move(yp, xp);
+    addch(ACS_LRCORNER);
+}
--- time-n-weather/numbers/numbers.h
+++ time-n-weather/numbers/numbers.h
@@ -7,4 +7,16 @@ void writeTwo(int x, int y);
 
 void writeThree(int x, int y);
 
+void writeFour(int x, int y);
+
+void writeFive(int x, int y);
+
+void writeSix(int x, int y);
+
+void writeSeven(int x, int y);
+
+void writeEight(int x, int y);
+
+void writeNine(int x, int y);
+
 
