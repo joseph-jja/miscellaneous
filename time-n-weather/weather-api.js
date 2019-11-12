a6d400485b41eeab743e46e8e1d82ff2266b0a51
Joe <josepha48@gmail.com>
2019-11-11 20:52:53 -0800

    basic untested impl

--- time-n-weather/weather-api.js
+++ time-n-weather/weather-api.js
@@ -2,12 +2,37 @@ const baseDir = process.cwd();
 
 const request = require('https').request;
 
-const API_ENDPOINT = 'https://api.weather.gov/';
+const API_HOSTNAME = 'api.weather.gov/';
 
 const latitude = '37.762626', 
      longitude = '-122.435291';
 
-const DISCOVER = `${API_ENDPOINT}/points/${latitude},${longitude}`;
+const USER_AGENT =`"Nozilla/1.0 (console; pios x86_64 ${process.version}) AppleWebKit/537.36 (KHTML, like Gecko but not) xpi/70.1.1`;
 
+const DISCOVER_ENDPOINT = `${API_ENDPOINT}/points/${latitude},${longitude}`;
+
+const options = {
+  hostname: API_HOSTNAME,
+  port: 443,
+  path: DISCOVER_ENDPOINT,
+  method: 'GET', 
+  headers: {
+      'User-Agent': USER_AGENT        
+  }
+};
+
+const req = https.request(options, (res) => {
+  console.log('statusCode:', res.statusCode);
+  console.log('headers:', res.headers);
+
+  res.on('data', (d) => {
+    process.stdout.write(d);
+  });
+});
+
+req.on('error', (e) => {
+  console.error(e);
+});
+req.end();
 
 
