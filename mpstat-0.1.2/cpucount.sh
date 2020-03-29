88c07eb0ce530e97bdb0f9d592a705d15b6f70b6
Joe <josepha48@gmail.com>
2020-03-28 18:50:50 -0700

    update generated code

--- mpstat-0.1.2/cpucount.sh
+++ mpstat-0.1.2/cpucount.sh
@@ -1,5 +1,15 @@
 #! /bin/sh
 
-grep ^processor /proc/cpuinfo | wc -l | awk '{ print "#define CPU_COUNT "$1 }' > cpucount.h
+CPU_NUMBER=`grep ^processor /proc/cpuinfo | wc -l `
+
+echo "#define CPU_COUNT $CPU_NUMBER" > cpucount.h
+
+echo "#define CPU_IDS char[$CPU_NUMBER]" >> cpucount.h
+
+echo "void init_cpu_ids();" >> cpucount.h
+
+echo "void init_cpu_ids()" > cpucount.c
+grep processor /proc/cpuinfo | sed 's/ //g' | awk -F\: '{print "\tCPU_IDS["$2"] = \42cpu"$2"\42;"}' >> cpucount.c
+echo "}" >> cpucount.c
 
 
