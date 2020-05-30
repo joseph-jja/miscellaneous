1dcc8d7d26cbf7b561ad67abbdc26f75301f8142
Joe <josepha48@gmail.com>
2020-05-30 12:51:39 -0700

    move request into separate file

---
+++ time-n-weather/node-libs/get.js
@@ -0,0 +1,56 @@
+const baseDir = process.cwd();
+
+const httpsRequest = require('https').request;
+
+const USER_AGENT = `"Nozilla/1.0 (console; pios x86_64 ${process.version}) AppleWebKit/537.36 (KHTML, like Gecko but not) xpi/70.1.1`;
+
+function getOptions(host, urlpath) {
+    const options = {
+        hostname: host,
+        port: 443,
+        path: urlpath,
+        method: 'GET',
+        headers: {
+            'User-Agent': USER_AGENT
+        }
+    };
+    return options;
+}
+
+async function request(options) {
+
+    return new Promise((resolve, reject) => {
+
+        const req = httpsRequest(options, (res) => {
+            //  console.log('statusCode:', res.statusCode);
+            //  console.log('headers:', res.headers);
+
+            const chunks = [];
+            res.on('data', (d) => {
+                chunks.push(d);
+            });
+
+            res.on('error', (e) => {
+                console.error(e);
+                reject(e);
+            });
+            res.on('end', () => {
+                const results = JSON.parse(Buffer.concat(chunks));
+
+                resolve(results);
+            });
+        });
+
+        req.on('error', (e) => {
+            console.error(e);
+            reject(e);
+        });
+        req.end();
+    });
+}
+
+
+module.exports = {
+    getOptions,
+    request
+};
--- time-n-weather/ow-weather-api.js
+++ time-n-weather/ow-weather-api.js
@@ -1,81 +1,42 @@
 const baseDir = process.cwd();
 
-const request = require('https').request, 
-    os = require('os'), 
-    fs = require('fs'), 
+const os = require('os'),
+    fs = require('fs'),
     {
         promisify
     } = require('util');
 
+const {
+    getOptions,
+    request
+} = require(`${baseDir}/node-libs/get`);
+
 const readFile = promisify(fs.readFile);
 
-const API_HOSTNAME = 'api.openweathermap.org';
+const latitude = '37.7464',
+    longitude = '-122.4442';
 
-const latitude = '37.7626',
-    longitude = '-122.4352';
+const API_HOSTNAME = 'api.openweathermap.org',
+    DISCOVER_ENDPOINT = `/data/2.5/weather?lat=${latitude}&lon=${longitude}`;
 
-const RELOAD = 1000 * 60 * 60; 
+const RELOAD = 1000 * 60 * 60;
 
-const USER_AGENT = `"Nozilla/1.0 (console; pios x86_64 ${process.version}) AppleWebKit/537.36 (KHTML, like Gecko but not) xpi/70.1.1`;
-
-const DISCOVER_ENDPOINT = `/data/2.5/weather?lat=${latitude}&lon=${longitude}`;
-
-const options = {
-    hostname: API_HOSTNAME,
-    port: 443,
-    path: DISCOVER_ENDPOINT,
-    method: 'GET',
-    headers: {
-        'User-Agent': USER_AGENT
-    }
-};
-
-async function makeRequest(options) {
-
-    const keyFile = await readFile(`${baseDir}/config.json`);
-    key = JSON.parse(keyFile.toString()).key;
-
-    options.path += `&APPID=${key}`;
-
-    return new Promise((resolve, reject) => {
-
-        const req = request(options, (res) => {
-            //  console.log('statusCode:', res.statusCode);
-            //  console.log('headers:', res.headers);
-
-            const chunks = [];
-            res.on('data', (d) => {
-                chunks.push(d);
-            });
-
-            res.on('error', (e) => {
-                console.error(e);
-                reject(e);
-            });
-            res.on('end', () => {
-                const results = JSON.parse(Buffer.concat(chunks));
-
-                resolve(results);
-            });
-        });
-
-        req.on('error', (e) => {
-            console.error(e);
-            reject(e);
-        });
-        req.end();
-    });
-}
 
 function toFahrenheit(kelvin) {
-    const raw = (9/5 * kelvin - 459.67);
+    const raw = (9 / 5 * kelvin - 459.67);
     return `${(Math.round(raw * 10) / 10)}F`;
 }
 
 async function start() {
 
     //const results = require('/tmp/full.js');
-    const results = await makeRequest(options);
+    const keyFile = await readFile(`${baseDir}/config.json`);
+    key = JSON.parse(keyFile.toString()).key;
+
+    const options = getOptions(API_HOSTNAME, DISCOVER_ENDPOINT);
+    options.path += `&APPID=${key}`;
+
+    const results = await request(options);
     const main = results.main;
 
     const temp = `Current: ${toFahrenheit(main.temp)}`;
@@ -84,21 +45,17 @@ async function start() {
 
     const tempRange = `Low: ${tempMin} - High: ${tempMax}`;
     const humidity = `Humidity: ${main.humidity}%`;
-    const cloudy =  `Clouds: ${results.clouds['all'] || 0}%`;
+    const cloudy = `Clouds: ${results.clouds['all'] || 0}%`;
     const rain = `Rain: ${results.rain && results.rain['1h'] || 0} / ${results.rain && results.rain['3h'] || 0}`;
 
     const data = `${temp}${os.EOL}${tempRange}${os.EOL}${humidity}${os.EOL}${cloudy}${os.EOL}${rain}${os.EOL}`;
 
-    fs.writeFile('/tmp/details.txt', data, () => {}) ;
-    fs.writeFile('/tmp/full.js', `module.exports = ${JSON.stringify(results)};`, () => {}) ;
+    fs.writeFile('/tmp/details.txt', data, () => {});
+    fs.writeFile('/tmp/full.js', `module.exports = ${JSON.stringify(results)};`, () => {});
 
 }
 
 start();
-setInterval(() => { 
+setInterval(() => {
     start();
 }, RELOAD);
-
-
-
-
