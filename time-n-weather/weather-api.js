d48ca0627302aa0113e6943f2b12e257bba06647
Joe Acosta <josepha48@gmail.com>
2020-06-02 17:03:58 -0700

    log more data

--- time-n-weather/weather-api.js
+++ time-n-weather/weather-api.js
@@ -25,44 +25,40 @@ const options = getOptions(API_HOSTNAME, DISCOVER_ENDPOINT);
 
 async function start() {
 
-    const props = await request(options);
-
-    if (!props || !props.properties || !props.properties.forecastHourly) {
-        return;
-    }
-
-    if (props.properties.forecastHourly.indexOf(API_HOSTNAME) < 0) {
-        return;
-    }
-    await writeFile('/tmp/forecast.json', JSON.stringify(props));
-
-    options.path = props.properties.forecastHourly.split(API_HOSTNAME)[1];
-    const forecast = await request(options);
-    //console.log(forecast);
-
-    let details = `Hourly: ${os.EOL}`;
-    forecast.properties.periods.filter((period, index) => {
-        // 8 hours only 
-        return (index < 8);
-    }).forEach(period => {
-        const startTime = period.startTime.split('T')[1].split('-')[0],
-            endTime = period.endTime.split('T')[1].split('-')[0];
-        details += `  (${startTime}-${endTime}): ${period.temperature}${period.temperatureUnit} / ${period.windSpeed} ${period.windDirection} ${os.EOL}`;
-    });
-
-    //console.log(details);
-    const err = await writeFile('/tmp/hourly.txt', details);
-}
-
-try {
-    start();
-} catch (e) {
-    console.log(e);
-}
-setInterval(() => {
     try {
-        start();
+        const props = await request(options);
+
+        await writeFile('/tmp/forecast.json', JSON.stringify(props));
+
+        if (!props || !props.properties || !props.properties.forecastHourly) {
+            return;
+        }
+
+        if (props.properties.forecastHourly.indexOf(API_HOSTNAME) < 0) {
+            return;
+        }
+        options.path = props.properties.forecastHourly.split(API_HOSTNAME)[1];
+        const forecast = await request(options);
+        await writeFile('/tmp/hourlyForecast.json', JSON.stringify(forecast));
+
+        let details = `Hourly: ${os.EOL}`;
+        forecast.properties.periods.filter((period, index) => {
+            // 8 hours only 
+            return (index < 8);
+        }).forEach(period => {
+            const startTime = period.startTime.split('T')[1].split('-')[0],
+                endTime = period.endTime.split('T')[1].split('-')[0];
+            details += `  (${startTime}-${endTime}): ${period.temperature}${period.temperatureUnit} / ${period.windSpeed} ${period.windDirection} ${os.EOL}`;
+        });
+
+        //console.log(details);
+        const err = await writeFile('/tmp/hourly.txt', details);
     } catch (e) {
         console.log(e);
     }
+}
+
+start();
+setInterval(() => {
+    start();
 }, RELOAD);
