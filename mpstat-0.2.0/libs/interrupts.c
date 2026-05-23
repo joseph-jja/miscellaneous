415f45e3b99e35eb49fb3d37d966f8a49b295059
Joe <josepha48@gmail.com>
2026-05-23 11:07:09 -0700

    Indentation

--- mpstat-0.2.0/libs/interrupts.c
+++ mpstat-0.2.0/libs/interrupts.c
@@ -31,38 +31,38 @@ get_inter (unsigned inters[], int number_cpus)
       // throw away frst line
       fgets (buffer, buffer_size, pFile);
       while (!feof (pFile)) {
-	fgets (buffer, buffer_size, pFile);
-	if (strncmp (buffer, "NMI: ", 5) == 0) {
-	  break;
-	}
-	char *idx = strstr (buffer, ":");
-	if (idx != NULL) {
-	  int p = strlen (idx + 1);
-	  j = 1;
-	  while (j < p && idx[j] == ' ') {
-	    j++;
-	  }
-	  k = 0;
-	  int x = 0, cpu_idx = 0;
-	  memset (tempbuffer, '\0', 12);
-	  while (j < p && cpu_idx < number_cpus) {
-	    if (idx[j] != ' ') {
-	      k = 1;
-	      tempbuffer[x] = idx[j];
-	      x++;
-	    } else {
-	      k = 0;
-	      if (x > 0) {
-		unsigned num = strtoul (tempbuffer, NULL, 0);
-		inters[cpu_idx] += num;
-		memset (tempbuffer, '\0', 12);
-		cpu_idx++;
-	      }
-	      x = 0;
-	    }
-	    j++;
-	  }
-	}
+		fgets (buffer, buffer_size, pFile);
+		if (strncmp (buffer, "NMI: ", 5) == 0) {
+		  break;
+		}
+		char *idx = strstr (buffer, ":");
+		if (idx != NULL) {
+		  int p = strlen (idx + 1);
+		  j = 1;
+		  while (j < p && idx[j] == ' ') {
+		    j++;
+		  }
+		  k = 0;
+		  int x = 0, cpu_idx = 0;
+		  memset (tempbuffer, '\0', 12);
+		  while (j < p && cpu_idx < number_cpus) {
+		    if (idx[j] != ' ') {
+		      k = 1;
+		      tempbuffer[x] = idx[j];
+		      x++;
+		    } else {
+		      k = 0;
+		      if (x > 0) {
+			unsigned num = strtoul (tempbuffer, NULL, 0);
+			inters[cpu_idx] += num;
+			memset (tempbuffer, '\0', 12);
+			cpu_idx++;
+		      }
+		      x = 0;
+		    }
+		    j++;
+		  }
+		}
       }
       free (tempbuffer);
       free (buffer);
