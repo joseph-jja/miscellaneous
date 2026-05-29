ea89e0d4ff4e889a8f60774002ff349977f3da7a
josepha48 <josepha48@gmail.com>
2026-05-28 21:14:15 -0700

    cleanups to get basics back

--- godura/src/builders/scrollarea.rs
+++ godura/src/builders/scrollarea.rs
@@ -1,24 +1,20 @@
-
-
-
 pub mod textarea {
 
     use crate::gtk;
-    use gtk::prelude::*;    
+    use gtk::prelude::*;
 
     pub fn create_gtk_buffer() -> gtk::TextBuffer {
         // 1. Create a TextBuffer to hold the content
         // don't need return but my thing
         return gtk::TextBuffer::new(None);
     }
-    
+
     pub fn build_text_area(buffer: gtk::TextBuffer) -> gtk::ScrolledWindow {
-        
         // 2. Create the TextView widget and assign the buffer
         let text_view = gtk::TextView::with_buffer(&buffer);
         //text_view.set_monospace(true); // Useful for code-like text
         text_view.set_wrap_mode(gtk::WrapMode::Word);
-    
+
         // 3. Wrap it in a ScrolledWindow to allow scrolling
         let scrolled_window = gtk::ScrolledWindow::builder()
             .child(&text_view)
@@ -27,7 +23,7 @@ pub mod textarea {
             .hexpand(true)
             .vexpand(true)
             .build();
-        
-        return scrolled_window
+
+        return scrolled_window;
     }
 }
