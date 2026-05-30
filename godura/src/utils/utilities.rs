cebebb8d7adcc9b983372f3b09ac3e99572ad17c
Joe <josepha48@gmail.com>
2026-05-30 13:48:12 -0700

    Update utilities.rs

--- godura/src/utils/utilities.rs
+++ godura/src/utils/utilities.rs
@@ -1,9 +1,15 @@
 pub mod utilities {
 
-  use crate::gtk;
-  use gtk::prelude::*;
+    use gtk4 as gtk;
+    use gtk::prelude::*;
+    use gtk::{  
+        Application,
+        Widget,
+        TextView,
+        TextBuffer
+    };
 
-  pub fn find_widget_by_name(parent: &gtk::Widget, name: &str) -> Option<gtk::Widget> {
+  pub fn find_widget_by_name(parent: &Widget, name: &str) -> Option<Widget> {
       // Check if the current widget matches the name
       if parent.widget_name() == name {
           return Some(parent.clone());
@@ -18,5 +24,20 @@ pub mod utilities {
           iter = child.next_sibling();
       }
       None
+  }
+
+  pub fn get_text_buffer(app: &Application) -> Option<TextBuffer> {
+
+    if let Some(window) = app.active_window() { 
+        let app_widget: Widget = window.upcast::<Widget>();
+        if let Some(text_widget) = find_widget_by_name(&app_widget, "main_text_buffer") {
+            let text_view = text_widget.downcast::<gtk::TextView>()
+                .expect("The widget was not a TextView");
+            
+            let buffer = text_view.buffer();
+            return Some(buffer);
+        }
+    }
+        return None;
   }
 }
