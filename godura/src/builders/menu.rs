9c76e4ae7fc46c3f0a98061e556e301f47a91c17
Joe <josepha48@gmail.com>
2026-05-30 18:56:20 -0700

    Update menu.rs

--- godura/src/builders/menu.rs
+++ godura/src/builders/menu.rs
@@ -34,7 +34,7 @@ pub mod menubar {
                             {
                                 let mut open_filename = current_filename_string()
                                     .write().unwrap();
-                                open_filename.push_str(&filename);
+                                open_filename.clear().push_str(&filename);
                             }
                         }
                     }
@@ -66,7 +66,7 @@ pub mod menubar {
                             {
                                 let mut open_filename = current_filename_string()
                                     .write().unwrap();
-                                open_filename.push_str(&filename);
+                                open_filename.clear().push_str(&filename);
                             }
                         }*/
                     }
