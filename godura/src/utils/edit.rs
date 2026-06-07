9ae4f05d1f65b65d1cc1686dad5cf96408ca12e0
Joe <josepha48@gmail.com>
2026-06-06 18:50:25 -0700

    Implement undo stack structure and functions
    Added a struct for undo stack and initialized related functions.

--- godura/src/utils/edit.rs
+++ godura/src/utils/edit.rs
@@ -1,1 +1,51 @@
 
+struct undostack {
+    let data: i8;
+    let type: i8;
+    let datalen: i32;
+    let pos: i32; 
+    let signalid: i32;
+};
+
+//typedef struct undostack UNDO_STACK;
+
+fn stack_init() {
+
+}
+
+fn stack_reset() {
+
+}
+
+fn stack_push() {
+
+}
+
+fn stack_pop() {
+
+}
+
+
+pub mod edit { 
+
+  pub fn insert_undo_stack() {
+
+  }
+
+  pub fn delete_undo_stack() {
+
+  }
+
+  pub fn cut_data() {
+
+  }
+
+  pub fn copy_data() {
+
+  }
+
+  pub fn paste_data() {
+
+  }
+
+}
