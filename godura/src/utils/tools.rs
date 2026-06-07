824f044fa5aeb3641aa0f6b0e84c9efb11940785
Joe <josepha48@gmail.com>
2026-06-06 18:54:06 -0700

    Add current_position_string function with RwLock

--- godura/src/utils/tools.rs
+++ godura/src/utils/tools.rs
@@ -1,1 +1,13 @@
 
+pub fn current_position_string() -> &'static RwLock<String> {
+    static STRING_LOCK: OnceLock<RwLock<String>> = OnceLock::new();
+    STRING_LOCK.get_or_init(|| RwLock::new(String::from("")))
+}
+
+pub mod buffer_utils {
+
+
+
+
+
+}
