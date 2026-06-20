75cd2e7a8301e13cb422ae230a011f01cf1a8c2b
Joe <josepha48@gmail.com>
2026-06-20 13:50:11 -0700

    Implement utility functions for weather data

--- time-n-weather/src/utils.js
+++ time-n-weather/src/utils.js
@@ -1,1 +1,39 @@
+pub mod utils {
 
+    use std::sync::{OnceLock, RwLock};
+  use std::fs::{read_to_string, write};
+
+    pub fn current_latitude() -> &'static RwLock<String> {
+        static STRING_LOCK: OnceLock<RwLock<String>> = OnceLock::new();
+        STRING_LOCK.get_or_init(|| RwLock::new(String::from("")))
+    }
+
+    pub fn current_longitude() -> &'static RwLock<String> {
+        static STRING_LOCK: OnceLock<RwLock<String>> = OnceLock::new();
+        STRING_LOCK.get_or_init(|| RwLock::new(String::from("")))
+    }
+
+    pub fn open_weathermap_api_key() -> &'static RwLock<String> {
+        static STRING_LOCK: OnceLock<RwLock<String>> = OnceLock::new();
+        STRING_LOCK.get_or_init(|| RwLock::new(String::from("")))
+    }
+
+     pub     fn read_in_file(filename: &String) -> String {
+
+        let contents = read_to_string(filename).expect("Could not read file specified!");
+
+        //println!("Open file {:?}", filename);
+
+        return contents;
+    }
+
+pub fn write_outfile(filename: &String, filedata: &String) -> Result<(), io::Error> {
+
+        println!("Saving file {:?}", filename);
+
+        write(filename, filedata)?;
+        Ok(())
+}
+
+
+}
