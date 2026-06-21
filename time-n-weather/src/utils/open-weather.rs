4eab5a739d4c391b469b8d92d5ec4652307ae635
Joe <josepha48@gmail.com>
2026-06-20 17:12:45 -0700

    Implement OpenWeather API data retrieval
    Added functionality to fetch weather data from OpenWeather API using current latitude and longitude.

--- time-n-weather/src/utils/open-weather.rs
+++ time-n-weather/src/utils/open-weather.rs
@@ -1,1 +1,41 @@
 
+pub mod open_weather {
+
+    use crate::utils::utils::utils::current_latitude;
+    use crate::utils::utils::utils::current_longitude;
+    use crate::utils::utils::utils::make_api_request;
+
+    const RELOAD: i32 = 1000 * 60 * 60;
+    const API_HOSTNAME: &str = "https://api.openweathermap.org";
+
+    pub fn get_open_weather_data() {
+
+        let mut endpoint: String = String::from(API_HOSTNAME);
+        endpoint.push_str("/data/2.5/weather?lat=");
+
+        {
+            let latitude = current_latitude().read().unwrap();
+            let borrowed_str: &str = &latitude;
+            endpoint.push_str(borrowed_str);
+        }
+
+        endpoint.push_str("&lon=");
+
+        {
+            let latitude = current_longitude().read().unwrap();
+            let borrowed_str: &str = &latitude;
+            endpoint.push_str(borrowed_str);
+        }
+
+        endpoint.push_str("&APPID=");
+        
+        {
+            let api_key = open_weathermap_api_key().read().unwrap();
+            let borrowed_str: &str = &api_key;
+            endpoint.push_str(borrowed_str);
+        }
+
+        let results = make_api_request(&endpoint);//.expect("Got weather data!");
+        println!("We got some results {:?}", results);
+    }
+}
