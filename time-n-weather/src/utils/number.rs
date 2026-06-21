0cc6a652a82a108e5c6f26da1cca13f2acb1497e
josepha48 <josepha48@gmail.com>
2026-06-20 20:21:25 -0700

    add basics parts for zero number

--- time-n-weather/src/utils/number.rs
+++ time-n-weather/src/utils/number.rs
@@ -7,6 +7,47 @@ pub mod numbers {
     pub fn write_zero(x: i16, y: i16) {
         let width: i16 = CLOCK_FONT_WIDTH - 2;
         let height: i16 = CLOCK_FONT_HEIGHT - 2;
+
+        let mut xp: u16 = x;
+        let mut yp: u16 = y;
+
+        let mut i: i8;
+
+        //move(yp, xp);
+
+        //addch(ACS_CKBOARD);
+        for i in 0..width {
+            xp += 1;
+            //move(yp, xp);
+            //addch(ACS_CKBOARD);
+        }
+        xp += 1;
+        //move(yp, xp);
+        //addch(ACS_CKBOARD);
+
+        for i in 0..height {
+            xp = x;
+            yp += 1;
+            //move(yp, xp);
+            //addch(ACS_CKBOARD);
+            xp += width;
+            xp += 1;
+            //move(yp, xp);
+            //addch(ACS_CKBOARD);
+        }
+
+        xp = x;
+        yp += 1;
+        //move(yp, xp);
+        //addch(ACS_CKBOARD);
+        for i in 0..width {
+            xp += 1;
+            //move(yp, xp);
+            //addch(ACS_CKBOARD);
+        }
+        xp += 1;
+        //move(yp, xp);
+        //addch(ACS_CKBOARD);
     }
 
     pub fn write_one(x: i16, y: i16) {
