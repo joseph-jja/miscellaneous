5f849b2511a6ee429f219c17c800a85ac9561d71
Joe <josepha48@gmail.com>
2026-06-20 17:34:38 -0700

    Add functions to write digits 0-9 for clock display

--- time-n-weather/src/utils/number.rs
+++ time-n-weather/src/utils/number.rs
@@ -1,1 +1,81 @@
+pub mod numbers {
 
+// width and height in this case 5x7
+const CLOCK_FONT_WIDTH: i16 = 5;
+const CLOCK_FONT_HEIGHT i16 = 7;
+
+
+  pub fn write_zero(x: i16, y: i16) {
+
+    let width: i16  = CLOCK_FONT_WIDTH - 2;
+    let height: i16 = CLOCK_FONT_HEIGHT - 2;
+
+
+  }
+
+    pub fn write_one(x: i16, y: i16) {
+
+    let width: i16  = CLOCK_FONT_WIDTH - 2;
+    let height: i16 = CLOCK_FONT_HEIGHT - 2;
+  }
+
+    pub fn write_two(x: i16, y: i16) {
+    
+      let width: i16  = CLOCK_FONT_WIDTH - 2;
+    let height: i16 = CLOCK_FONT_HEIGHT - 2;
+
+  }
+
+    pub fn write_three(x: i16, y: i16) {
+
+      let width: i16  = CLOCK_FONT_WIDTH - 2;
+    let height: i16 = CLOCK_FONT_HEIGHT - 2;
+
+  }
+
+    pub fn write_four(x: i16, y: i16) {
+
+          let width: i16  = CLOCK_FONT_WIDTH - 2;
+    let height: i16 = CLOCK_FONT_HEIGHT - 2;
+
+  }
+
+    pub fn write_five(x: i16, y: i16) {
+
+          let width: i16  = CLOCK_FONT_WIDTH - 2;
+    let height: i16 = CLOCK_FONT_HEIGHT - 2;
+
+  }
+
+    pub fn write_six(x: i16, y: i16) {
+
+          let width: i16  = CLOCK_FONT_WIDTH - 2;
+    let height: i16 = CLOCK_FONT_HEIGHT - 2;
+
+  }
+
+    pub fn write_seven(x: i16, y: i16) {
+
+          let width: i16  = CLOCK_FONT_WIDTH - 2;
+    let height: i16 = CLOCK_FONT_HEIGHT - 2;
+
+  }
+
+    pub fn write_eight(x: i16, y: i16) {
+
+          let width: i16  = CLOCK_FONT_WIDTH - 2;
+    let height: i16 = CLOCK_FONT_HEIGHT - 2;
+
+  }
+
+    pub fn write_nine(x: i16, y: i16) {
+
+          let width: i16  = CLOCK_FONT_WIDTH - 2;
+    let height: i16 = CLOCK_FONT_HEIGHT - 2;
+
+  }
+
+
+
+
+}
