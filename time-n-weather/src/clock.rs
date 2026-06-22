a378683080cff723d81f7ac8ab45fa43e7d7f64c
Joe <josepha48@gmail.com>
2026-06-21 17:17:00 -0700

    Refactor minute handling in clock.rs
    Refactor minute calculation to remove unnecessary type casting.

--- time-n-weather/src/clock.rs
+++ time-n-weather/src/clock.rs
@@ -46,9 +46,9 @@ fn write_time() {
             number::write(5 + 11 + 11 + 2, 2, 0);
             number::write(5 + 11 + 11 + 11 + 2, 2, minute);
         } else {
-            let minute_hundred = floor(minute / 10, 0) as u16;
-            let minute_ten = (minute - (minute_hundred * 10)) as u16;
-            number::write(5 + 11 + 11 + 2, 2, minute_hundred);
+            let minute_hundred = floor(minute / 10, 0);
+            let minute_ten = (minute - (minute_hundred * 10));
+            //number::write(5 + 11 + 11 + 2, 2, minute_hundred);
             //number::write(5 + 11 + 11 + 11 + 2, 2, minute_ten);
         }
 
