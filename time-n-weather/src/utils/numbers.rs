17877fd21c84b5a0671823545cfb06ff9b031d49
Joe <josepha48@gmail.com>
2026-06-22 18:31:33 -0700

    Add write_colon function for clock display
    Added a new function 'write_colon' to handle clock formatting.

--- time-n-weather/src/utils/numbers.rs
+++ time-n-weather/src/utils/numbers.rs
@@ -339,6 +339,16 @@ pub mod numbers {
         }
     }
 
+    pub fn write_colon(x: u16, y: u16) {
+
+        let width: u16 = CLOCK_FONT_WIDTH - 1;
+        let height: u16 = CLOCK_FONT_HEIGHT - 1;
+
+        let width_middle: u16 = (width / 2);
+        let heightwidth_middle: u16 = (height / 2);
+
+    }
+
     pub fn get_offset(index: u16, left_offset: u16) -> u16 {
         let offset: u16 = (index * (CLOCK_FONT_WIDTH + 2)) + left_offset;
         return offset;
