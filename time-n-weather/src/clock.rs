ffb65b651cf7afcd069d93fe29ecb622d09d2010
Joe <josepha48@gmail.com>
2026-06-23 18:14:22 -0700

    Refactor clock.rs to use write_colon function
    Commented out the write_text_at function and added a note to use write_colon instead.

--- time-n-weather/src/clock.rs
+++ time-n-weather/src/clock.rs
@@ -60,7 +60,8 @@ fn write_time() {
             number::write(hour_small, 2, hour_ten);
         }
 
-        write_text_at(hour_small + 4, 5, &"*");
+        // need to use write_colon function for this
+        //write_text_at(hour_small + 4, 5, &"*");
 
         if minute < 10 {
             number::write(minute_big, 2, 0);
