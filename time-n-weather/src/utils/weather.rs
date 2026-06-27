79ff622900c6181509760b42e76ded89620a630d
Joe <josepha48@gmail.com>
2026-06-26 18:35:37 -0700

    Add hourly forecast data structure and parsing
    Added HourlyForecastData struct and updated weather data parsing to include hourly forecast details.

--- time-n-weather/src/utils/weather.rs
+++ time-n-weather/src/utils/weather.rs
@@ -2,7 +2,7 @@ pub mod weather {
 
     use std::path::PathBuf;
 
-    //use serde::{Deserialize, Serialize};
+    use serde::{Deserialize, Serialize};
     use serde_json::Value;
 
     use crate::utils::utils::utils::current_latitude;
@@ -11,6 +11,16 @@ pub mod weather {
     use crate::utils::utils::utils::write_outfile;
 
     const API_HOSTNAME: &str = "https://api.weather.gov";
+
+    #[derive(Serialize, Deserialize, Clone, Copy, Default, Debug)]
+    struct HourlyForecastData {
+        startTime: String,
+        endTime: String,
+        temperature: String,
+        temperatureUnit: String,
+        windSpeed: String,
+        windDirection: String
+    };
 
     pub fn get_weather_data() {
         let mut endpoint: String = String::from(API_HOSTNAME);
@@ -57,6 +67,18 @@ pub mod weather {
             forecast_filename.push("hourlyForecast.json");
             let forecast_name: String =  forecast_filename.to_string_lossy().into_owned();
             let _ = write_outfile(&forecast_name, &hourly_forcast);
+
+            let hourly_data: Value = serde_json::from_str(&hourly_forcast.as_str()).expect("Should have hourly forecast data!");
+
+            let mut four_hour_forecast: [HourlyForecastData; 4] = [HourlyForecastData::default(); 4];
+            if let Some(periods) = hourly_data.get("properties").and_then(|d| d.get("periods")) { 
+                for index in 0..4 {
+                    let Some(a_period) = periods.get(&index.as_str()) {
+                        let hour_data: HourlyForecastData = serde_json::from_str(a_period.as_str()).expect("Could not parse hourly forcast line!");
+                        four_hour_forecast[index] = hour_data;
+                    }
+                }
+            }
 
             // TODO parse out first 4 
             // startTime, endTime, (in 2026-06-29T08:00:00-07:00 => out 08:00:00)
