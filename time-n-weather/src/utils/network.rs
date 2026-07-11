772c986b02aa4fdfc07a12f07424990178cb5394
Joe <josepha48@gmail.com>
2026-07-11 13:20:12 -0700

    Add network interface retrieval functionality
    Implemented a module to retrieve network interfaces and their IP addresses.

---
+++ time-n-weather/src/utils/network.rs
@@ -0,0 +1,24 @@
+pub mod iface {
+    use network_interface::{NetworkInterface, NetworkInterfaceConfig};
+  
+    use crate::utils::utils::utils::LINE_ENDING;
+    
+    pub fn get_interfaces() -> String {
+        // Retrieve all network interfaces on the system
+        let interfaces = NetworkInterface::show().expect("Failed to load network interfaces");
+  
+        let mut network_info: String = String::from("");
+        for interface in interfaces {
+            network_info.push_str(interface.name);
+            
+            // Loop through all associated IP addresses (IPv4 and IPv6)
+            network_info.push_str(":");
+            for addr in interface.addr {
+                network_info.push_str(" ");
+                network_info.push_str(addr.ip());
+            }
+            network_info.push_str(LINE_ENDING);
+        }
+        return network_info;
+    }
+}
