c6eeaace1bd3442860bc6ad639b844939e7c23b8
Joe <josepha48@gmail.com>
2026-09-19 10:21:10 -0700

    Refactor read_temp_file function for error handling
    Updated the return type of read_temp_file to use a custom Error type and simplified the return statement.

--- time-n-weather/src/utils/utils.rs
+++ time-n-weather/src/utils/utils.rs
@@ -101,14 +101,14 @@ pub mod utils {
         let _ = write_outfile(&output_name, &data);
     }
 
-    pub fn read_temp_file(filename: &str) -> Result<String, io::Error> {
+    pub fn read_temp_file(filename: &str) -> Result<String, Error> {
         let mut input_filename = PathBuf::new();
         input_filename.push("/");
         input_filename.push("tmp");
         input_filename.push(&String::from(filename));
         let input_name: String = input_filename.to_string_lossy().into_owned();
 
-        let results = read_to_string(filename)?;
-        Ok(results);
+        return read_to_string(filename)?;
+        //Ok(results);
     }
 }
